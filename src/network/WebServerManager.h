6624e888501bf6ccfe14d6e946aabeae7b809074
Hhaltt <105934938+Hhaltt@users.noreply.github.com>
2026-08-26 15:06:43 +0300

    Add chat management web handlers

--- src/network/WebServerManager.h
+++ src/network/WebServerManager.h
@@ -3,55 +3,32 @@
 #include <Arduino.h>
 #include <WebServer.h>
 
-
 class WebServerManager
 {
 public:
-
     void begin();
-
     void update();
 
-
 private:
-
     WebServer server;
 
-
-    // --------------------------------------------------------
-    // PAGES
-    // --------------------------------------------------------
-
     void handleRoot();
-
     void handleSettings();
-
     void handleSaveSettings();
-
+    void handleAddChat();
+    void handleDeleteChat();
     void handleNotFound();
 
     void handleReminders();
-
     void handleAddReminder();
-
     void handleDeleteReminder();
-
     void handleToggleReminder();
 
     void handleHubTelegramSend();
 
-    // --------------------------------------------------------
-    // HELPERS
-    // --------------------------------------------------------
-
-    String getPageHeader(
-        const String& title
-    );
-
+    String getPageHeader(const String& title);
     String getPageFooter();
-
     String getNavigation();
 };
-
 
 extern WebServerManager webServerManager;
