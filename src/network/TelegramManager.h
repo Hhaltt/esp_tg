c3cda951a138cb10b4ce02b8865e02d793c3d275
Hhaltt <105934938+Hhaltt@users.noreply.github.com>
2026-08-26 15:05:06 +0300

    Add silent Telegram message support

--- src/network/TelegramManager.h
+++ src/network/TelegramManager.h
@@ -11,17 +11,21 @@ public:
 
     bool sendMessage(
         const String& chatId,
-        const String& text
+        const String& text,
+        bool silent = false
     );
 
 private:
     bool started = false;
+    bool startupMessageSent = false;
     unsigned long lastCheck = 0;
+
     void checkMessages();
     void handleMessage(int messageIndex);
     void handleCommand(const String& chatId, const String& command);
     void sendStartMessage(const String& chatId);
     void sendAbout(const String& chatId);
+    void sendStartupMessage();
 };
 
 extern TelegramManager telegram;
