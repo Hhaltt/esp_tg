6eb49c292bdc443a549996b2022cd4eeb650bc02
Hhaltt <105934938+Hhaltt@users.noreply.github.com>
2026-08-26 15:01:56 +0300

    Support silent Telegram notifications

--- src/network/TelegramManager.h
+++ src/network/TelegramManager.h
@@ -2,67 +2,27 @@
 
 #include <Arduino.h>
 
-
 class TelegramManager
 {
 public:
-
     void begin();
-
     void update();
-
-
     bool isStarted();
-
-
-    // --------------------------------------------------------
-    // Send messages
-    // --------------------------------------------------------
 
     bool sendMessage(
         const String& chatId,
-        const String& text
+        const String& text,
+        bool silent = false
     );
-
 
 private:
-
     bool started = false;
-
-    bool startupMessageSent = false;
-
     unsigned long lastCheck = 0;
-
-
     void checkMessages();
-
-    void handleMessage(
-        int messageIndex
-    );
-
-
-    // Commands
-
-    void handleCommand(
-        const String& chatId,
-        const String& command
-    );
-
-
-    void sendStartMessage(
-        const String& chatId
-    );
-
-
-    void sendAbout(
-        const String& chatId
-    );
-
-
-    // Startup message
-
-    void sendStartupMessage();
+    void handleMessage(int messageIndex);
+    void handleCommand(const String& chatId, const String& command);
+    void sendStartMessage(const String& chatId);
+    void sendAbout(const String& chatId);
 };
-
 
 extern TelegramManager telegram;
