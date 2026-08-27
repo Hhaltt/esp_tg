352d0a5730775d95908fa785769261e41866685f
Hhaltt <105934938+Hhaltt@users.noreply.github.com>
2026-08-27 12:42:09 +0300

    Add hardware timed RX TX LED manager

---
+++ src/services/LedActivityManager.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+#include <esp_timer.h>
+
+class LedActivityManager
+{
+public:
+    void begin();
+    void update();
+    void onRx();
+    void onTx();
+
+private:
+    esp_timer_handle_t rxTimer = nullptr;
+    esp_timer_handle_t txTimer = nullptr;
+    uint64_t lastMessagesReceived = 0;
+    uint64_t lastMessagesSent = 0;
+
+    static void rxTimerCallback(void* arg);
+    static void txTimerCallback(void* arg);
+};
+
+extern LedActivityManager ledActivity;
