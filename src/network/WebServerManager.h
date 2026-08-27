7ec90c77f334158c1268824c94f8e95dec0af440
Hhaltt <105934938+Hhaltt@users.noreply.github.com>
2026-08-27 18:09:39 +0300

    Declare command route web handlers

--- src/network/WebServerManager.h
+++ src/network/WebServerManager.h
@@ -1,35 +1,15 @@
 #pragma once
-
 #include <Arduino.h>
 #include <WebServer.h>
-
 class WebServerManager
 {
 public:
-    void begin();
-    void update();
-
-    WebServer& rawServer()
-    {
-        return server;
-    }
-
+    void begin(); void update(); WebServer& rawServer(){return server;}
 private:
     WebServer server;
-
-    void handleSaveSettings();
-    void handleAddChat();
-    void handleUpdateChat();
-    void handleDeleteChat();
-
-    void handleAddReminder();
-    void handleUpdateReminder();
-    void handleCloneReminder();
-    void handleDeleteReminder();
-    void handleToggleReminder();
-
-    void handleHubTelegramSend();
-    void handleNotFound();
+    void handleSaveSettings(); void handleAddChat(); void handleUpdateChat(); void handleDeleteChat();
+    void handleAddCommandRoute(); void handleEditCommandRoute(); void handleDeleteCommandRoute();
+    void handleAddReminder(); void handleUpdateReminder(); void handleCloneReminder(); void handleDeleteReminder(); void handleToggleReminder();
+    void handleHubTelegramSend(); void handleNotFound();
 };
-
 extern WebServerManager webServerManager;
